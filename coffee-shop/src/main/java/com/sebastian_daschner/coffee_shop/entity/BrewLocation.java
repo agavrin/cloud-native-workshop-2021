2b04c16d0b21405ba468baffaa216b0b49e61797
Sebastian Daschner <git@sebastian-daschner.de>
2020-04-17 10:57:07 +0200

    inital commit; added workshop contents;

---
+++ coffee-shop/src/main/java/com/sebastian_daschner/coffee_shop/entity/BrewLocation.java
@@ -0,0 +1,15 @@
+package com.sebastian_daschner.coffee_shop.entity;
+
+import java.util.stream.Stream;
+
+public enum BrewLocation {
+    DACH,
+    CEE;
+
+    public static BrewLocation of(String string) {
+        return Stream.of(BrewLocation.values())
+                .filter(t -> t.name().equalsIgnoreCase(string))
+                .findAny().orElse(null);
+    }
+
+}
