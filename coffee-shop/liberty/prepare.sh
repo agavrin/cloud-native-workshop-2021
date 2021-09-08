5e8bc470a942d52a8a271a555a71efc6f7ec700d
Alexander gavrin <agavrin@gmail.com>
2021-09-08 12:32:47 +0300

    Revert "revert changes back"
    This reverts commit c1ef22ff30a79a18fb319d3828d32b90bc8b8711.

--- coffee-shop/liberty/prepare.sh
+++ coffee-shop/liberty/prepare.sh
@@ -4,7 +4,7 @@ set -eu
 
 # avoid storing the jar files in VCS
 
-curl -L https://github.com/WASdev/sample.opentracing.zipkintracer/releases/download/2.0.1/liberty-opentracing-zipkintracer-2.0.1-sample.zip \
- -o liberty-opentracing-zipkintracer-2.0.1-sample.zip
-unzip liberty-opentracing-zipkintracer-2.0.1-sample.zip
-rm liberty-opentracing-zipkintracer-2.0.1-sample.zip
+curl -L https://github.com/WASdev/sample.opentracing.zipkintracer/releases/download/1.3/liberty-opentracing-zipkintracer-1.3-sample.zip \
+ -o liberty-opentracing-zipkintracer-1.3-sample.zip
+unzip liberty-opentracing-zipkintracer-1.3-sample.zip
+rm liberty-opentracing-zipkintracer-1.3-sample.zip
